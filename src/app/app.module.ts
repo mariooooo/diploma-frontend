e887b81abb5f623dbf46608e24f3a6d4941f61ef
vitaliy <k.vitaliy1994@gmail.com>
2018-02-05 23:46:59 +0200

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {Routes, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 
 import {MatButtonModule} from '@angular/material/button';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { LecturersListComponent } from './components/lecturers-list/lecturers-list.component';
 
+const appRoutes: Routes =[
+  { path: '', component: HomeComponent},
+  { path: 'lecturers', component: LecturersListComponent}
+  // { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    LecturersListComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     MatButtonModule,
     BrowserModule
   ],
